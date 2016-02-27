aa43b8cdc9e9bcfa75ac594c6e1fdd532e4a7bb7
do7be <bomsap700@gmail.com>
2016-02-27 15:31:32 +0900

    add before and after func

--- spec/LogicTest.js
+++ spec/LogicTest.js
@@ -1,13 +1,33 @@
 describe("TestSample>", function() {
-    describe("Logic>", function() {
-        it("multiNumber", function() {
-            var target = new Logic();
-            var num = 3;
-            var expected = 9;
-
-            var result = target.squaredNumber(num);
-
-            expect(expected).toEqual(result);
-        })
+  describe("Logic>", function() {
+    beforeEach(function() {
+      console.log('test ready >>>');
     });
+
+    afterEach(function() {
+      console.log("<<< test end");
+    });
+
+    it("multiNumber plus", function() {
+      var target = new Logic();
+      var num = 3;
+      var expected = 9;
+
+      var result = target.squaredNumber(num);
+
+      console.log(`input:${num} expected:${expected} result:${result}`);
+      expect(expected).toEqual(result);
+    })
+
+    it("multiNumber minus", function() {
+      var target = new Logic();
+      var num = -3;
+      var expected = 9;
+
+      var result = target.squaredNumber(num);
+
+      console.log(`input:${num} expected:${expected} result:${result}`);
+      expect(expected).toEqual(result);
+    })
+  });
 });
